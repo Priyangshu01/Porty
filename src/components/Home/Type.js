5bf560dd3ea9a3ba8d0dfd8e4db331806e1cd3a0
Priyangshu01 <118904204+Priyangshu01@users.noreply.github.com>
2023-05-12 21:41:28 +0530

    Type.js

--- src/components/Home/Type.js
+++ src/components/Home/Type.js
@@ -7,8 +7,8 @@ function Type() {
       options={{
         strings: [
           "Developer",
-          "Deep Learning Engineer",
-          "MERN Stack Developer",
+          "Deep Learner",
+          "Student Life",
           "Open Source Contributor",
         ],
         autoStart: true,
